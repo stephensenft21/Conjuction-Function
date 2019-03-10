c923f6ef1fc6ef16e83185e876b740e666682039
Stephen Senft <stephensenft21@gmail.com>
2019-03-10 17:08:05 -0500

    finished exercise

---
+++ src/script/main.js
@@ -0,0 +1,9 @@
+
+
+let conjoinedWord = "";
+const conjunction = function (firstWord, secondWord) {
+     conjoinedWord = `${firstWord} ${secondWord}`
+};
+
+conjunction("Master", "Card")
+console.log(conjoinedWord)
